133bbd6e257e71d6e717255cf18d381e9efb9bd7
annanderiba <annandejene2020@gmail.com>
2025-11-02 20:07:45 +0400

    Added opcode to clear result from ALU

--- ALU.v
+++ ALU.v
@@ -4,6 +4,8 @@ module ALU(num1, num2, opcode, result);
     input [3:0] opcode; // 4 bit operation code
     output reg [15:0] result;
 
+    // wire zero, carry, overflow, negative;
+
     always @(num1 or num2 or opcode)
         case (opcode)
             4'b0001: result = num1 + num2; // ADD
@@ -11,7 +13,13 @@ module ALU(num1, num2, opcode, result);
             4'b0011: result = num1 & num2; // AND
             4'b0100: result = num1 | num2;  // OR
             4'b0101: result = num1 ^ num2;  // XOR
-            default: result = 0;            // default case incase opcode is not any of the above
+            4'b0110: result = 0;            // Clear
+            default: result = num1;         // default case incase opcode is not any of the above -- bypass simply give num1
         endcase 
+
+    // assign negative = result[15];
+    // assign zero = (result == 0);
+    // assign carry = (num1[15] & num2[15])
+
 
 endmodule
