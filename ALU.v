6d3bd3a91189a0f3306fc2720022fcadaa3e950e
annanderiba <annandejene2020@gmail.com>
2025-11-03 03:56:09 +0400

    added zero condition flag

--- ALU.v
+++ ALU.v
@@ -1,8 +1,9 @@
-module ALU(num1, num2, opcode, result);
+module ALU(num1, num2, opcode, result, zero);
     input [15:0] num1; // 16 bit register inputs to ALU
     input [15:0] num2;
     input [3:0] opcode; // 4 bit operation code
     output reg [15:0] result;
+    output zero;
 
     // wire zero, carry, overflow, negative;
 
@@ -14,14 +15,10 @@ module ALU(num1, num2, opcode, result);
             4'b0100: result = num1 | num2;  // OR
             4'b0101: result = num1 ^ num2;  // XOR
             4'b0110: result = ~num1; // NOT the first register
-            4'b0111: result = 0;            // Clear
-
             default: result = num1;         // default case incase opcode is not any of the above -- bypass simply give num1
         endcase 
 
-    // assign negative = result[15];
-    // assign zero = (result == 0);
-    // assign carry = (num1[15] & num2[15])
+    assign zero = (result == 0);
 
 
 endmodule
