bef041404ba2d17d9aa08ac8428574be5d3b5ffb
annanderiba <annandejene2020@gmail.com>
2025-11-20 17:58:22 +0400

    fixed issues regarding the display of memory content

--- memory.v
+++ memory.v
@@ -1,33 +1,49 @@
 module memory(clock, enable, writeEnable, address, writeData, readData);
     input clock, enable, writeEnable;
-    input [15:0] address;
+    input [7:0] address;
     input [15:0] writeData;
-    output reg [15:0] readData;
+    output wire [15:0] readData;
 
     // Initially we planned to make use of all the bits for the memory -> 65,536 x 16-bit memory array -> but this was too much to work with so we reduced it to 256 
-    reg [15:0] mem[0:16];  // but we are going to make it 256 memory locations X each storing 16-bit
+    reg [15:0] mem[0:255];  // but we are going to make it 256 memory locations X each storing 16-bit
 
     // load memory content from program.hex
-    integer i;
     initial begin
-        for (i = 0; i < 65536; i = i + 1) // first clear all to 0
-            mem[i] = 16'h0000;
-      $readmemh("program.hex", mem);  // Then load 16-bit words, one per line
+        $readmemh("program.hex", mem);  // Then load 16-bit words, one per line
+        display_memory_content(); // begin by displaying the memory content in the file
+        // $writememh("memory_content.txt", mem); // alternative
+
     end
 
-    // variables for writing memory content to a file
-    integer f;
-    integer j;
+    task display_memory_content(); 
+        // variables for writing memory content to a file
+        integer f; // file handler
+        integer j; // memory location counter
+
+        begin
+            f = $fopen("memory_content.txt", "w"); // open the file for writing
+            for (j = 0; j < 256; j++) begin
+                $fwrite(f, "%02d: %04h\n", j[7:0], mem[j]);  // addr: data
+            end
+            $fclose(f); // close the file        
+        end
+    endtask
+
 
     // write the content of the RAM memory to a file
     always @ (posedge clock) begin
-        if (writeEnable) begin
+        if (enable && writeEnable) begin
             mem[address] <= writeData; // write to the RAM at given address
-            f = $fopen("memory_content.txt", "w"); // open the file for writing
-            for (i = 0; i < 256; i++) begin
-                $fwrite(f, "%h \n", mem[i]); // write the data to the file
-            end
-            $fclose(f); // close the file
+            // #1 $writememh("memory_content.txt", mem); // alternative
+
+            // #1 display_memory_content(); // update the memory content file accordingly
+        end
+    end
+
+    // taken out of the above always block to avoid a race condition between the displaying and writing to the RAM which would be scheduled for the same time (posedge of the clock) so we write to the RAM on the posedge of the clock and display the updates on the following negedge
+    always @(negedge clock) begin
+        if (enable && writeEnable) begin
+            display_memory_content();
         end
     end
 
@@ -42,19 +58,7 @@ module memory(clock, enable, writeEnable, address, writeData, readData);
     end
 */
 
-    // read from and write to memory when enabled
-    always @(posedge clock) begin
-        if (enable && writeEnable)
-            mem[address] <= writeData;
-    end
-
     // asynchronous read when enabled 
-    always @* begin
-        if (enable)
-            readData = mem[address]; //--> read from the RAM at given address
-        else
-            readData = 16'h0000; // if not enabled out put 0
-    end
-    
+    assign readData = enable ? mem[address] : 16'h0000;
 
 endmodule
--- memory_tb.v
+++ memory_tb.v
@@ -1,12 +1,15 @@
 `include "memory.v"
 
 module memory_tb;
-    reg [15:0] address, writeData;
+    reg [7:0] address;
+    reg [15:0] writeData;
     reg clock = 1'b1, enable, writeEnable;
 
     wire [15:0] readData;
 
     memory myMemory(.clock(clock), .enable(enable), .writeEnable(writeEnable), .address(address), .writeData(writeData), .readData(readData));
+
+    always #5 clock = ~clock;
 
     initial begin
     // generate files needed to plot the waveform using GTKWave
@@ -16,71 +19,60 @@ module memory_tb;
         // assign values with time to input signals to see output
         enable = 1'b1;
         writeEnable = 1'b0;
+        address = 8'h00;
 
-// ---- Reading ------
-        #10
-        address = 8'h00; // memory[0]
+        // Wait for the first negative edge to start operations -- basically #5 but synchronizes it well
+        @(negedge clock); 
 
-        #10
-        address = 8'h01; // memory[1]
 
-        #10
-        address = 8'h02; // memory[2]
+        // ---- Reading - read the first 5 elements from the memory ------
+        // We set up the address on the negative edge, 
+        // the module reads the data on the *next* positive edge.
+        repeat (4) begin 
+            @(negedge clock); address = address + 1'b1;
+        end
+        // At this point, several clock cycles have passed and memory_content.txt has been initialized once.
 
-        #10
-        address = 8'h03; // memory[3]
-        
-// ---- Writing -------
-        #10
-        writeEnable = 1'b1;
-        enable = 1'b1;
-        
-        #10
-        address = 8'b0000_1111;
-        writeData = 16'hFA2D; // memory[15] = 0xFA2D;
 
-        #10
-        writeEnable = 1'b0;
+// ---- Writing 1 -------
+        @(negedge clock); 
+        writeEnable = 1'b1; // Turn on write mode
+        address = 8'h0F;    // Address 15
+        writeData = 16'hFA2D; 
 
-        #10
-        address = 8'b0000_1111; // read val stored in memory[5]
+        @(negedge clock);   // Wait for one more negative edge (the write happens on the posedge in between)
+        writeEnable = 1'b0; // Turn off write mode
 
+        @(negedge clock);
+        address = 8'h0F;    // Read back address 15 (check readData wire after posedge clock)
 
         // 2nd write 
-        #10
+        @(negedge clock); 
         writeEnable = 1'b1;
-        enable = 1'b1;
-        
-        #10
-        address = 8'b0001_0100;
-        writeData = 16'h2231; // memory[20] = 0xFA2D;
+        address = 8'h0C;    // Address 12
+        writeData = 16'h2231; 
 
-        #10
+        @(negedge clock); 
         writeEnable = 1'b0;
 
-        #10
-        address = 8'b0001_0100; // read val stored in memory[5]
+        @(negedge clock);
+        address = 8'h0C;    // Read back address 12
 
         // 3rd write 
-        #10
+        @(negedge clock); 
         writeEnable = 1'b1;
-        enable = 1'b1;
-        
-        #10
-        address = 8'b0001_0111;
-        writeData = 16'h9999; // memory[23] = 0xFA2D;
+        address = 8'h14;    // Address 20
+        writeData = 16'h9999; 
 
-        #10
+        @(negedge clock); 
         writeEnable = 1'b0;
 
-        #10
-        address = 8'b0001_0111; // read val stored in memory[5]
+        @(negedge clock);
+        address = 8'h14;    // Read back address 20
 
-        #20
+        @(negedge clock);
+        #1;
+        $display("Simulation finished.");
         $finish;
     end
-
-
-    always #5 clock = ~clock;
-
 endmodule
