0c6cd888a0b05aa6c79bf2f6119f3e47a3980146
annanderiba <annandejene2020@gmail.com>
2025-11-20 15:25:32 +0400

    adjusted the memory test bench to check the new functionality added to the memory module

--- memory_tb.v
+++ memory_tb.v
@@ -19,32 +19,64 @@ module memory_tb;
 
 // ---- Reading ------
         #10
-        address = 16'h0000; // memory[0]
+        address = 8'h00; // memory[0]
 
         #10
-        address = 16'h0001; // memory[1]
+        address = 8'h01; // memory[1]
 
         #10
-        address = 16'h0002; // memory[2]
+        address = 8'h02; // memory[2]
 
         #10
-        address = 16'h0003; // memory[3]
+        address = 8'h03; // memory[3]
         
 // ---- Writing -------
         #10
         writeEnable = 1'b1;
+        enable = 1'b1;
         
         #10
-        address = 16'h0005; 
-        writeData = 16'hFA2D; // memory[5] = 0xFA2D;
+        address = 8'b0000_1111;
+        writeData = 16'hFA2D; // memory[15] = 0xFA2D;
 
         #10
         writeEnable = 1'b0;
 
         #10
-        address = 16'h0005; // read val stored in memory[5]
+        address = 8'b0000_1111; // read val stored in memory[5]
+
+
+        // 2nd write 
+        #10
+        writeEnable = 1'b1;
+        enable = 1'b1;
+        
+        #10
+        address = 8'b0001_0100;
+        writeData = 16'h2231; // memory[20] = 0xFA2D;
 
         #10
+        writeEnable = 1'b0;
+
+        #10
+        address = 8'b0001_0100; // read val stored in memory[5]
+
+        // 3rd write 
+        #10
+        writeEnable = 1'b1;
+        enable = 1'b1;
+        
+        #10
+        address = 8'b0001_0111;
+        writeData = 16'h9999; // memory[23] = 0xFA2D;
+
+        #10
+        writeEnable = 1'b0;
+
+        #10
+        address = 8'b0001_0111; // read val stored in memory[5]
+
+        #20
         $finish;
     end
 
