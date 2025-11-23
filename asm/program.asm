c97039cc7316b178734bda9021da2bd47cdf7199
annanderiba <annandejene2020@gmail.com>
2025-11-24 01:11:56 +0400

    sample program to run

--- asm/program.asm
+++ asm/program.asm
@@ -1,9 +1,11 @@
 MOV R1, #0 ; sum = 0
 MOV R2, #10 ; i = 10
 MOV R3, #1 ; val = 1
+MOV R4, #15 ; memory address we will write to
 
 ADD R1, R1, R2 ; sum += i
 SUB R2, R2, R3 ; i--
-BEQZ R2, 0x07 ;; (R2 == 0 ??) yes -> exit 
-JMP 0x03 ;; else loop back
+BEQZ R2, 0x08 ;; (R2 == 0 ??) yes -> exit 
+JMP 0x04 ;; else loop back
+STR R4, R1 ;; memory[15]
 HLT ;; end (expect R1 = 55)
